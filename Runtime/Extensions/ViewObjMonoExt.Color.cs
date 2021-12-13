a413e13362486bdc12dc57d8ca7cd6d6f511da1d
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-12-13 17:47:36 -0600

    purged converter scripts

---
+++ Runtime/Extensions/ViewObjMonoExt.Color.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ViewTo.Objects.Mono.Extensions
+{
+  public static partial class ViewObjMonoExt
+  {
+    
+    public static ViewColor ToView(this Color32 value, int index)
+    {
+      return new ViewColor(value.r, value.g, value.b, value.a, index);
+    }
+
+    public static Color32 ToUnity(this ViewColor value)
+    {
+      return value != null ? new Color32(value.R, value.G, value.B, value.A) : default;
+    }
+  }
+}
---
+++ Runtime/Extensions/ViewObjMonoExt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using ViewTo.Connector.Unity;
+
+namespace ViewTo.Objects.Mono.Extensions
+{
+  public static partial class ViewObjMonoExt
+  {
+    public static int GetCount(this IViewCloud obj) => obj.points.Valid() ? obj.points.Length : 0;
+
+    public static string FullName(this ViewContentMono obj) => obj.GetRef?.TypeName()[0] + "-" + obj.viewName;
+
+  }
+}
